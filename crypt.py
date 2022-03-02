9409b11c630aa62316cb508c1eab206873e44fb2
Jake Nemiroff <jakenemiroff@10-17-218-68.dynapool.wireless.nyu.edu>
2022-03-02 12:02:53 -0500

    update to crypt.py file to map ciphertext to expected plaintext

--- crypt.py
+++ crypt.py
@@ -1,31 +1,55 @@
 import ast
 from ctypes import sizeof
 
-inp = input()
+def ciphertext_to_plaintext(ciphertext_dict, plaintext_dict):
+
+    plaintext = ""
+
+    for letter in ciphertext_dict.keys():
+
+        letter_freq = ciphertext_dict.get(letter)
+
+        for plaintext_key, plaintext_freq in plaintext_dict.items():
+            if plaintext_freq == letter_freq:
+                print(plaintext_key + ' -----> ' + str(plaintext_freq))
+                break
+
+    return plaintext
 
 
-all_freq = {}
-s = {}
-for i in inp:
-    if i in all_freq:
-        all_freq[i] += 1
-    else:
-        all_freq[i] = 1 
-print(sorted(all_freq))
-keys = all_freq.values()
-nkeys = list(keys)
-print (nkeys)
-f = open("freq.txt", "r")
-new_freq = f.readlines()
-for j in new_freq:
-    s = ast.literal_eval(j)
-    k = s.values()
-    # print(k)
-    nk = list(k)
-    print(nk)
-    # if nkeys == nk:
-    #     for l in inp:
-    #         print(l.replace(str(all_freq.keys), str(s.keys)))
-    #     print("The plaintext is: ")
-    # else:
-    #     print("Nahh")
+def main():
+    inp = input()
+
+    result =''
+    ciphertext = {}
+    s = {}
+    for i in inp:
+        if i in ciphertext:
+            ciphertext[i] += 1
+        else:
+            ciphertext[i] = 1 
+    # print(ciphertext)
+    # print(sorted(ciphertext))
+    frequency = ciphertext.values()
+    nfreq = list(frequency)
+    # print (nkeys)
+    f = open("freq.txt", "r")
+    plaintext_frequency = f.readlines()
+    for line in plaintext_frequency:
+        plaintext_line = ast.literal_eval(line)
+        print(plaintext_line)
+        # print(nk)
+        # if nkeys == nk:
+        #     for l in inp:
+        #         print(l.replace(str(ciphertext.keys), str(s.keys)))
+        #     print("The plaintext is: ")
+        # else:
+        #     print("Nahh")
+
+        result += ciphertext_to_plaintext(ciphertext, plaintext_line)
+
+    print(result)
+
+    f.close()
+
+main()
